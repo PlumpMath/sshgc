ad45128af448ccfde709fb54fb62becf08cdf2e4
coldnew <coldnew.tw@gmail.com>
2013-06-03 00:46:03 +0800

    add Xnest and Xephyr command

--- src/sshgc/controller.clj
+++ src/sshgc/controller.clj
@@ -4,13 +4,41 @@
            [javafx.scene Scene]
            [javafx.event ActionEvent]
            [javafx.scene.text Text])
-  (:require [sshgc.dialog :as dialog]))
+  (:require [sshgc.dialog :as dialog])
+  (:use [clojure.java.shell :only [sh]]))
 
-(defn dependancy-missing []
-  (dialog/error "Dependance Missing" "Please install Xnet or Xeyphr")
-  )
+(defn dependancy-missing
+  "Show dependancy missing dialog."
+  []
+  (dialog/error "Dependance Missing" "Please install Xnet or Xeyphr"))
 
+(defn command-exist?
+  "Check if command exist on system."
+  [x]
+  (> 2 (:exit (sh x))))
 
-;; (defn create-display []
-;;   (let ((app "Xnest"))
-;;   )
+(def xephyr-exist?
+  (command-exist? "Xephyr"))
+
+(defn xephyr
+  "Xephry commands."
+  [window-size disp]
+  (let [size (if (= window-size "fullscreen") "-fullscreen"
+                 (str "-screen " window-size))]
+  (str "Xephyr -ac " size " -br -reset -terminate 2> /dev/null " ":" disp " &")))
+
+(defn xnest
+  "Xnest commands."
+  [window-size disp]
+  (str "Xnest -geometry " window-size " :" disp " &"))
+
+(defn create-display
+  "Create display if Xnest or Xephry exist."
+  [window-size disp]
+  (let [d (str ":" disp)
+        run-sh (fn [x] (future (sh "bash" "-c" x)))]
+
+    (cond (command-exist? "Xephyr") (run-sh (xephyr window-size disp))
+          (command-exist? "Xnest") (run-sh (xnest window-size disp))
+          :else (dependancy-missing))
+    ))
